6f0d36a9e731fc9626c52c50f086a882fb00ea6e
Dave Abrahams <dabrahams@adobe.com>
2023-11-09 10:35:16 -0800

    Make sure we get typealias Float16 = Half when we need it

--- Sources/CBORCoding/Half.swift
+++ Sources/CBORCoding/Half.swift
@@ -1,6 +1,7 @@
 import Foundation
 
-#if canImport(Half) && swift(<5.3)
+#if canImport(Half) && swift(<5.3) || (
+  os(macOS) || targetEnvironment(macCatalyst)) && arch(x86_64)
 import Half
 
 typealias Float16 = Half
